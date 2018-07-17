3661f3c2a684f6a017b4e6c921cd421523e19812
Unknown <vaibhav.sharma@athenasoft.in>
2018-07-17 17:06:04 +0530

    oauth  Changes
    Registration For Facebook Implementation

--- registerwithfacebook.h
+++ registerwithfacebook.h
@@ -3,18 +3,21 @@
 
 #include <QObject>
 #include <QDebug>
+#include <QtNetworkAuth/QOAuthHttpServerReplyHandler>
+#include <QtNetworkAuth/QOAuth2AuthorizationCodeFlow>
 
 class RegisterWithFacebook : public QObject
 {
     Q_OBJECT
 public:
-
+    QOAuth2AuthorizationCodeFlow oauth2;
     explicit RegisterWithFacebook(QObject *parent = nullptr);
     ~RegisterWithFacebook();
 
 public slots:
-
+    void onStatusChanged(QAbstractOAuth::Status status);
     void onRegisterWithFbClicked();
+    void onAuthorizeWithBrowser(QUrl url);
 
 };
 
